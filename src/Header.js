814f6c808d495eeefaa995e999852df79395b1b1
Carlos Eduardo <carloseduardolimabastosm@gmail.com>
2021-01-20 04:54:57 -0300

    criando a pagina Header

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div></div>
+        <header className="header-color">
+            <section>
+                <a href="/" className="logo">logo</a>
+                    <ul>
+                        <li><a href="/">home</a></li>
+                        <li><a href="/">sobre</a></li>
+                        <li><a href="/">informação</a></li>
+                    </ul>
+            </section>
+        </header>
     )
 }
 
