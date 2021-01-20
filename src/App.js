1e6d6212e2c82ddb3d5b6c5d2cd9fea3bc9d1914
Carlos Eduardo <carloseduardolimabastosm@gmail.com>
2021-01-20 04:47:19 -0300

    importando a pagina header e configurando o lottie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-
+import animationData from './assets/lotties/43885-laptop-working.json'
+import Header from './Header';
 
 
 function App() {
